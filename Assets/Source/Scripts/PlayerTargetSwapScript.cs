b508266a3f0156792fe1b68dee85006a9c078c1d
edemskiy <qapple@ya.ru>
2019-02-28 18:03:09 +0300

    исправлен сброс цвета меток при очистке маршрута

--- Assets/Source/Scripts/PlayerTargetSwapScript.cs
+++ Assets/Source/Scripts/PlayerTargetSwapScript.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerTargetSwapScript : MonoBehaviour
 {
     public NavMeshController navMeshController;
+    public LabelsController labelsController;
     public Button ButtonFrom;
     public Button ButtonTo;
 
@@ -24,7 +26,23 @@ public class PlayerTargetSwapScript : MonoBehaviour
     public void Clear()
     {
         navMeshController.ResetPath();
+        GameObject labelFrom = labelsController
+            .GetLabelObjectByName(ButtonFrom.GetComponentInChildren<Text>().text);
+        GameObject labelTo = labelsController
+            .GetLabelObjectByName(ButtonTo.GetComponentInChildren<Text>().text);
+
+        if(labelFrom != null)
+        {
+            labelFrom.GetComponent<TextMeshPro>().color = Color.white;
+            
+        }
+        if (labelTo != null)
+        {
+            labelTo.GetComponent<TextMeshPro>().color = Color.white;
+        }
+
         ButtonFrom.GetComponentInChildren<Text>().text = AppUtils.ButtonFrom_DefaultName;
         ButtonTo.GetComponentInChildren<Text>().text = AppUtils.ButtonTo_DefaultName;
+        
     }
 }
