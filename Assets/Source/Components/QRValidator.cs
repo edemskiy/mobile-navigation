b325cf44bc886640212ca16760ff136396438319
edemskiy <qapple@ya.ru>
2019-03-07 17:04:26 +0300

    добавлены комментарии к обработчику qr-меток

--- Assets/Source/Components/QRValidator.cs
+++ Assets/Source/Components/QRValidator.cs
@@ -7,11 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class QRValidator : MonoBehaviour
 {
-    public Text nameText;
-    public Text aboutText;
-    public GameObject serviceWindow;
-    public GameObject qrLabelInfo;
-    private UnityAction<string> qrDetectedListener;
+    public Text audienceName; // поле для вывода названия помещения
+    public Text audienceInfo; // поле для вывода информации о помещении
+    public GameObject serviceWindow; // окно для вывода ошибок
+    public GameObject qrInfoWindow; // окно с информацией о помещении
+
+    private UnityAction<string> qrDetectedListener; 
     private string nameString, aboutString;
     private string labelJSON;
     private bool qrFound;
@@ -38,6 +39,7 @@ public class QRValidator : MonoBehaviour
         EventManager.StopListening(AppUtils.qrDetected, qrDetectedListener);
     }
 
+    // обработчик qr-метки
     private void OnQrDetected(string s)
     {
         // проверка валидности маркера
@@ -47,18 +49,28 @@ public class QRValidator : MonoBehaviour
             info.HasField(AppUtils.JSON_FLOOR))
         {
             labelJSON = s;
+
+            // прямое присваивание в текстовые поля интерфейса не работает;
+            // скорее всего из-за асинхронности обработки qr-метки
             nameString = info.GetField(AppUtils.JSON_NAME).str;
             aboutString = info.GetField(AppUtils.JSON_INFO).str;
             qrFound = true;
         }
         else
         {
+            // если метка не валидна то выводим сообщение с ошибкой
             serviceWindow.SetActive(true);
         }
     }
 
+    // обработчик для кнопок перехода на сцену с навигацией
     public void ShowMapButtonOnClick()
     {
+        /* если маркер найден, то записываем инфу в PlayerPrefs,
+         * что бы потом ее можно было считать в другой сцене.
+         * Иначе - очищаем все поля в PlayerPrefs
+         */
+    
         if (qrFound)
         {
             PlayerPrefs.SetString(AppUtils.JSON_QR, labelJSON);
@@ -67,19 +79,21 @@ public class QRValidator : MonoBehaviour
         {
             PlayerPrefs.DeleteAll();
         }
-
+        
+        // переходим на сцену навигации
         SceneManager.LoadScene(AppUtils.NavigationSceneName);
     }
 
     public void CloseButtonOnClick()
     {
         qrFound = false;
-        qrLabelInfo.gameObject.SetActive(false);
+        qrInfoWindow.gameObject.SetActive(false);
     }
+
     void Update()
     {
-        nameText.text = nameString;
-        aboutText.text = aboutString;
-        qrLabelInfo.gameObject.SetActive(qrFound);
+        audienceName.text = nameString;
+        audienceInfo.text = aboutString;
+        qrInfoWindow.gameObject.SetActive(qrFound);
     }
 }
