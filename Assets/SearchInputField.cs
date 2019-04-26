995a23f4dc23295efae91aac7e21b90fb65a7e36
edemskiy <qapple@ya.ru>
2019-04-26 13:37:04 +0300

    изменен интерфейс окна поиска

---
+++ Assets/SearchInputField.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchInputField : MonoBehaviour
+{
+    public Button clearButton;
+    private InputField inputField;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+    public void OnValueChanged()
+    {
+        clearButton.gameObject.SetActive(inputField.text.Length > 0);
+    }
+}
